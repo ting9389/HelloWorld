ce2e77ee078e7105db98ff044612cf4832d8cd61
ting9389 <xutingwang0420@gmail.com>
2024-07-17 20:31:31 +0900

    Java_P21

---
+++ ChangeChar.java
@@ -0,0 +1,20 @@
+public class ChangeChar{
+	
+	public static void main(String[] arg) {
+		//	\t:一个制位表，可实现对齐的功能
+		System.out.println("北京\t天津\t上海");
+		//	\n:换行符
+		System.out.println("猫\nです。");
+		//	\\:一个\
+		System.out.println("http:\\www");
+		//	\\\\:两个\\
+		System.out.println("http:\\\\goole.com\\\\");
+		//	\":一个"
+		System.out.println("java学习\"");
+		//	\':一个'
+		System.out.println("xuexi'");
+		//	\r:一个回车
+		System.out.println("社长在直播\r我们");
+		
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	
+	
+	public static void main(String[] arg) {
+		System.out.println("私は今javaを勉強しています。");
+	}
+	
+}
