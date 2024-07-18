c0deb4a2c0c300e04540dc4d468fe3d6b3103da2
ting9389 <xutingwang0420@gmail.com>
2024-07-18 18:28:56 +0900

    java_27

---
+++ ChangeCharExer01.java
@@ -0,0 +1,6 @@
+public class ChangeCharExer01{
+	
+	public static void main(String[] arg) {
+		System.out.println("D:\\javacode>java Test\n书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
+	}
+}
---
+++ JavaDocComment.java
@@ -0,0 +1,18 @@
+//	多行注释：/* */
+//	文档注释：/** */
+
+/**
+ * @author dreamtank
+ * @version 1.0
+ */
+public class JavaDocComment{
+	
+	/*
+	 * 多行注释
+	 * 多行注释
+	 */
+	public static void main(String[] arg) {
+		System.out.println("多行注释");
+	}
+		
+}
